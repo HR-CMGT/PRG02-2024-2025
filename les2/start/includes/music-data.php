5e81136a09900f7a0f5bcee09208c255eda2a95a
Martijn Willemse <mewillemse@gmail.com>
2024-11-20 09:51:28 +0100

    Renamed an index of the musicalbums

--- les2/start/includes/music-data.php
+++ les2/start/includes/music-data.php
@@ -2,70 +2,70 @@
 $musicAlbums = [
     [
         'artist' => 'Adele',
-        'name' => '30',
+        'album' => '30',
         'genre' => 'Pop',
         'year' => '2021',
         'tracks' => '12'
     ],
     [
         'artist' => 'Muse',
-        'name' => 'Live At Rome Olympic Stadium',
+        'album' => 'Live At Rome Olympic Stadium',
         'genre' => 'Rock',
         'year' => '2013',
         'tracks' => '13'
     ],
     [
         'artist' => 'Dream Theater',
-        'name' => 'Systematic Chaos',
+        'album' => 'Systematic Chaos',
         'genre' => 'Progressive Rock',
         'year' => '2007',
         'tracks' => '8'
     ],
     [
         'artist' => 'Hardwell',
-        'name' => 'United We Are',
+        'album' => 'United We Are',
         'genre' => 'House',
         'year' => '2015',
         'tracks' => '14'
     ],
     [
         'artist' => 'Robbie Williams',
-        'name' => 'Greatest Hits',
+        'album' => 'Greatest Hits',
         'genre' => 'Pop',
         'year' => '2010',
         'tracks' => '57'
     ],
     [
         'artist' => 'Limp Bizkit',
-        'name' => 'Gold Cobra',
+        'album' => 'Gold Cobra',
         'genre' => 'Rock / Rap',
         'year' => '2011',
         'tracks' => '16'
     ],
     [
         'artist' => 'Angels & Airwaves',
-        'name' => 'Love Part 1',
+        'album' => 'Love Part 1',
         'genre' => 'Rock',
         'year' => '2011',
         'tracks' => '11'
     ],
     [
         'artist' => 'Ed Sheeran',
-        'name' => '=',
+        'album' => '=',
         'genre' => 'Pop',
         'year' => '2021',
         'tracks' => '14'
     ],
     [
         'artist' => 'Kygo',
-        'name' => 'Cut Your Teeth',
+        'album' => 'Cut Your Teeth',
         'genre' => 'Chillstep',
         'year' => '2014',
         'tracks' => '3'
     ],
     [
         'artist' => '30 Seconds To Mars',
-        'name' => 'This Is War',
+        'album' => 'This Is War',
         'genre' => 'Rock',
         'year' => '2009',
         'tracks' => '15'
